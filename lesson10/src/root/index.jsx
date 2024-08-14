03766f4cf1c3c4e65ee8504291168b075ebc4865
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-08-14 23:20:15 +0500

    lesson10 LocalStorage

--- lesson10/src/root/index.jsx
+++ lesson10/src/root/index.jsx
@@ -1,9 +1,29 @@
 import Table from "../components/Table/index";
 import Navbar from "../components/Navbar/index";
+import { useState } from "react";
 
 export const Root = () => {
+  const [state, setState] = useState(
+    Number(localStorage.getItem("count") || 1)
+  );
+
+  const onPlus = () => {
+    localStorage.setItem("count", state + 1);
+    setState(state + 1);
+  };
+
+  const onMinus = () => {
+    localStorage.setItem("count", state - 1);
+    setState(state - 1);
+  };
+
   return (
     <div>
+      <h2>{state}</h2>
+      <button onClick={onPlus}>+</button>
+      <button onClick={onMinus}>-</button>
+      <button onClick={() => localStorage.removeItem("count")}>Remove</button>
+      <button onClick={() => localStorage.clear()}>Clear</button>
       <Navbar />
       <Table />
     </div>
@@ -11,3 +31,8 @@ export const Root = () => {
 };
 
 export default Root;
+
+//set
+//get
+//remove
+//clear
